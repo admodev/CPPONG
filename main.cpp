3eefe9de55e62dfd28cb208bde9c8226f4c8df2e
admodev <admodevcodes@outlook.com>
2022-06-22 22:05:37 -0300

    add entity and colors constants

--- main.cpp
+++ main.cpp
@@ -4,16 +4,22 @@ int main(void)
 {
 	const int screenWidth = 800;
 	const int screenHeight = 450;
+	const Color screenBackgroundColor = BLACK;
+	const Color entityColor = WHITE;
 
 	InitWindow(screenWidth, screenHeight, "Pong!");
 	SetWindowState(FLAG_VSYNC_HINT);
 	SetTargetFPS(60);
 
+	float ballX = GetScreenWidth() / 2.0f;
+	float ballY = GetScreenHeight() / 2.0f;
+	float ballRadius = 5;
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-			ClearBackground(BLACK);
-			DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE);
+			ClearBackground(screenBackgroundColor);
+			DrawCircle((int) ballX, (int) ballY, ballRadius, entityColor);
 			DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
 			DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
 			DrawFPS(10, 10);
