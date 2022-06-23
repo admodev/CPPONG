50c4cfa3064d006d7c19357e820f82d366cd5f1d
admodev <admodevcodes@outlook.com>
2022-06-22 21:55:47 -0300

    add rectangles

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ int main(void)
 	{
 		BeginDrawing();
 			ClearBackground(BLACK);
+			DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE);
+			DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
+			DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
 			DrawFPS(10, 10);
 		EndDrawing();
 	}
